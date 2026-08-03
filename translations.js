4925bc2ef1ef9a82338c44b2c638c32e5e7b0867
BambiOrb <damian.yanez.antelo@gmail.com>
2026-08-03 17:53:50 +0200

    Update translations.js

--- translations.js
+++ translations.js
@@ -261,7 +261,7 @@ const STB_TRANSLATIONS = {
     contact_sending: 'Sending…',
 
     // EN
-    promo_img: "img/promos/promo-aug-en.PNG", promo_beauty: "Beauty",
+    promo_img: "img/promos/promo-aug-en.png", promo_beauty: "Beauty",
     
     welcome_badge: "Opening: 8 June 2026",
     welcome_kicker: "New at STB Atelier",
@@ -406,7 +406,7 @@ const STB_TRANSLATIONS = {
     contact_sending: 'Invio…',
 
     // IT
-    promo_img: "img/promos/promo-aug-it.jpeg", promo_beauty: "Beauty",
+    promo_img: "img/promos/promo-aug-it.jpg", promo_beauty: "Beauty",
 
     welcome_badge: "Apertura: 8 Giugno 2026",
     welcome_kicker: "Novità da STB Atelier",
